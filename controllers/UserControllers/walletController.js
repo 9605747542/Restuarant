8ef5067a7e550d363406f573099221a2b617c0a5
nourin <nourinvn@gmail.com>
2024-06-05 08:54:08 +0530

    wallet done

--- controllers/UserControllers/walletController.js
+++ controllers/UserControllers/walletController.js
@@ -3,9 +3,18 @@ const Walletdb=require('../../models/UserModels/walletSchema');
 const Userdb=require('../../models/UserModels/UserSignupSchema');
 userwallet.getwalletpage=async(req,res)=>{
     const data=await Walletdb.findOne({userId:req.session.userid})
+   
     const user=await Userdb.findOne({_id:req.session.userid});
     const userName=user.name;
-    res.render('userViews/walletdetails',{data,userName})
+    if(data){
+        res.render('userViews/usercart', {data,userName, message: '' });
+    }else{
+       
+            res.render('userViews/usercart', { datas: [],userName, message: 'No Wallet found in Wallets.' });
+        
+
+    }
+  
 }
 
 
