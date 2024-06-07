af743c781a125bed0dd36a0ac324dc8a9a9f194b
nourin <nourinvn@gmail.com>
2024-06-07 10:07:09 +0530

    setting adminlogin

--- controllers/AdminControllers/adminlogin.js
+++ controllers/AdminControllers/adminlogin.js
@@ -5,29 +5,29 @@ const admin=require('../../models/AdminModels/AdminloginSchema');
 const adminlogin={};
 const isAdminLogged=require('../../middleware/adminMiddle');
 
-// adminlogin.adminregister=async(req, res) => {
-//     try {
-//         const {email1,password1}=req.body;
-//         console.log("cat",email1);
-//         console.log("pass",password1);
-//         const hashedpassword = await bcrypt.hash(password1, 10); 
-//         console.log(hashedpassword);// Use the provided password for hashing
-//         const newUser = new admin({
-//             email: email1,
-//             password: hashedpassword
-//         });
-//         await newUser.save();
+adminlogin.adminregister=async(req, res) => {
+    try {
+        const {email1,password1}=req.body;
+        console.log("cat",email1);
+        console.log("pass",password1);
+        const hashedpassword = await bcrypt.hash(password1, 10); 
+        console.log(hashedpassword);// Use the provided password for hashing
+        const newUser = new admin({
+            email: email1,
+            password: hashedpassword
+        });
+        await newUser.save();
 
-//         if (newUser) {
-//             res.json({ message: "Successfully Saved the data" });
-//         } else {
-//             res.json({ message: "Failed to Save the data" });
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Internal Server Error" });
-//     }
-// };
+        if (newUser) {
+            res.json({ message: "Successfully Saved the data" });
+        } else {
+            res.json({ message: "Failed to Save the data" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
 adminlogin.getlogin=async(req,res)=>{
 const {email1,password1}=req.body;
 
